94bcd32660dcbf574e3956078fb27fc611f8b573
Jesper Swärd <sward.jesper@gmail.com>
2026-03-24 12:00:06 +0100

    Added Event model for calendar  and SQL database

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Create an SQL-file
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///planner.db"
+db = SQLAlchemy(app) # Connect db to the Flask-app
+
+# Create class which defines a table in db.
+class Event(db.Model):
+    id          = db.Column(db.Integer, primary_key = True) #Unique identifier for each activity
+    title       = db.Column(db.String(100)) # <-- Title, max 100 chars
+    date        = db.Column(db.String(20))
+    start_time  = db.Column(db.String(10))
+    end_time    = db.Column(db.String(10))
+    description = db.Column(db.Text)
 
 @app.route("/")
 def home():
@@ -8,4 +22,7 @@ def home():
 
 
 if __name__ == "__main__":
+    # If db does not exist, create it:
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
