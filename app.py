9d37504883a831851cc0d3b0f8ff8af62d42a59e
Jesper Swärd <sward.jesper@gmail.com>
2026-03-24 12:45:36 +0100

    Updated working form connected to SQL-database

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+#render_template shows HTML-pages
+#request reads data from forms
+#redirect sends user to another page
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -16,6 +19,34 @@ class Event(db.Model):
     end_time    = db.Column(db.String(10))
     description = db.Column(db.Text)
 
+
+# Adds route for the event form
+@app.route("/addevent", methods=["GET", "POST"])
+def add_event():
+    if request.method == "POST":            # Has user sent the form?
+        title       = request.form["title"]
+        date        = request.form["date"]
+        start_time  = request.form["start_time"]
+        end_time    = request.form["end_time"]
+        description = request.form["description"]
+
+        new_event = Event(
+            title = title,
+            date = date,
+            start_time = start_time,
+            end_time = end_time,
+            description = description
+            )
+        
+        # Save userdata to db
+        db.session.add(new_event) # Add object in session
+        db.session.commit()        # Write to SQLite
+
+        return redirect("/") # Sends user back to homepage after save
+    
+    return render_template("addevent.html")
+
+# Adds route to Homepage
 @app.route("/")
 def home():
     return render_template("index.html")
