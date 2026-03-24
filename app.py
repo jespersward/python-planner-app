e2574c0738f8831647faa2d71528f28ec0ebc5a5
Jesper Swärd <sward.jesper@gmail.com>
2026-03-24 14:30:42 +0100

    Added a button to edit an event. CRUD-cycle completed

--- app.py
+++ app.py
@@ -19,6 +19,9 @@ class Event(db.Model):
     end_time    = db.Column(db.String(10))
     description = db.Column(db.Text)
 
+    def __repr__(self):
+        return f"<Event {self.title}>"
+
 
 # Adds route for the event form
 @app.route("/addevent", methods=["GET", "POST"])
@@ -49,12 +52,38 @@ def add_event():
 # Adds route to Homepage
 @app.route("/")
 def home():
-    events = Event.query.all() # Retrieves all rows from Event-table
+    events = Event.query.order_by(Event.date, Event.start_time).all() # Retrieves all rows from Event-table in correct order
     return render_template("index.html", events = events) # Render index.html and give access to variable "events"
 
+# Adds delete-route
+@app.route("/delete/<int:event_id>")
+def delete_event(event_id):
+    event = Event.query.get_or_404(event_id) # Retrieve event with ID, if non-exsistant show 404-error
+    db.session.delete(event)
+    db.session.commit()
+    return redirect("/")
+
+@app.route("/edit/<int:event_id>", methods=["GET", "POST"])
+def edit_event(event_id):
+    event = Event.query.get_or_404(event_id)
+
+    if request.method == "POST":
+        event.title         = request.form["title"]
+        event.date          = request.form["date"]
+        event.start_time    = request.form["start_time"]
+        event.end_time      = request.form["end_time"]
+        event.description   = request.form["description"]
+
+        db.session.commit()
+
+        return redirect("/")
+    return render_template("edit_event.html", event = event)
 
 if __name__ == "__main__":
     # If db does not exist, create it:
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
+
+
